a968ebdf2ef1d6c7224924d73916a783d001c878
Nicolas <nicolas_p_souza@estudante.sc.senai.br>
2023-06-21 22:45:21 -0300

    Correcoes

--- src/pages/Menu/index.js
+++ src/pages/Menu/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import Card from "../Card";
 import Home from "../Home";
 import Store from "../Store";
@@ -9,7 +10,7 @@ import Preferences from "../Preferences";
 
 const Tab = createBottomTabNavigator();
 
-export default function Menu() {
+export default function Menu({route}) {
  
  return (
     
@@ -28,10 +29,27 @@ export default function Menu() {
            },
          }}>
           
-            <Tab.Screen name="Home" component={Home} options={{headerShown:false}}/>
-            <Tab.Screen name="Card" component={Card} options={{headerShown:false}} />
-            <Tab.Screen name="Store" component={Store} options={{headerShown:false}}/>
-            <Tab.Screen name="Preferences" component={Preferences} options={{headerShown:false}}/>
+            <Tab.Screen name="Home" component={Home} options={{
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="card-account-details-outline" color={color} size={size} />
+          ),headerShown:false
+        }}
+      /> 
+            <Tab.Screen name="Card" component={Card} options={{
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="credit-card-check-outline" color={color} size={size} />
+          ),headerShown:false
+        }} />
+            <Tab.Screen name="Store" component={Store} options={{
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="gift-outline" color={color} size={size} />
+          ),headerShown:false
+        }}/>
+            <Tab.Screen name="Preferences" component={Preferences} options={{
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="power-cycle" color={color} size={size} />
+          ),headerShown:false
+        }}/>
         </Tab.Navigator>
     </NavigationContainer>
   );
